af59656207d8167947d5b82739664e56f011a38e
Himanshu Goswami <himanshu.goswami@e2erspl.COM>
2021-08-26 19:05:35 +0530

    home page popup done

--- src/components/StaticDashboard/index.tsx
+++ src/components/StaticDashboard/index.tsx
@@ -8,61 +8,61 @@ import {
 import { Link } from "react-router-dom";
 import DoshboardChart from "../../assets/svg/dashboard-chart.svg";
 import LocalStorageUtils from "../../utils/LocalStorageUtils";
+import CustomScrollbar from "../CustomScrollbar";
 
 const StaticDashboard: React.FC = () => {
   const user = LocalStorageUtils.getUser();
   return (
     <div className="info-area">
-      <h2>Don’t react, anticipate demand</h2>
-      <ul>
-        <li>
-          “HFS OneOffice Pulse” is designed to focus on anticipated demand
-          changes for technology and business services.  We envision it to be
-          the ultimate semi-annual guide to technology and business services
-        </li>
-        <li>
-          Organizations that will thrive in the pandemic economy must be in step
-          with and ultimately anticipate customer requirements and expectations.
-          They will need to become pre-emptive in every sense of the word.
-        </li>
-      </ul>
-      <Grid container>
-        <Grid item md={6} sm={6} className="info-area__imgwider">
-          <img src={DoshboardChart} />
+      <CustomScrollbar>
+        <h2>Don’t react, anticipate demand</h2>
+        <ul>
+          <li>
+            “HFS OneOffice Pulse” is designed to focus on anticipated demand
+            changes for technology and business services.  We envision it to be
+            the ultimate semi-annual guide to technology and business services
+          </li>
+          <li>
+            Organizations that will thrive in the pandemic economy must be in
+            step with and ultimately anticipate customer requirements and
+            expectations. They will need to become pre-emptive in every sense of
+            the word.
+          </li>
+        </ul>
+        <Grid container>
+          <Grid item md={6} sm={6} className="info-area__imgwider">
+            <img src={DoshboardChart} />
+          </Grid>
+          <Grid item md={6} sm={6}>
+            <h2>Demographics:</h2>
+            <ul>
+              <li>
+                800 respondents across the enterprises with more than $500M
+                revenues
+              </li>
+              <li>45% North America, 35% Europe, 20% RoW</li>
+              <li>60%+ C-level respondents</li>
+              <li>60% business stakeholders and 40% IT stakeholders</li>
+              <li>
+                Roughly equal split across major industries that HFS tracks
+                incl. BFS; insurance; healthcare & life sciences; Technology,
+                media & telecom; energy & utilities; retail & CPG; industrial
+                manufacturing; and travel, hospitality, & logistics
+              </li>
+            </ul>
+          </Grid>
         </Grid>
-        <Grid item md={6} sm={6}>
-          <h2>Demographics:</h2>
-          <ul>
-            <li>
-              800 respondents across the enterprises with more than $500M
-              revenues
-            </li>
-            <li>45% North America, 35% Europe, 20% RoW</li>
-            <li>60%+ C-level respondents</li>
-            <li>60% business stakeholders and 40% IT stakeholders</li>
-            <li>
-              Roughly equal split across major industries that HFS tracks incl.
-              BFS; insurance; healthcare & life sciences; Technology, media &
-              telecom; energy & utilities; retail & CPG; industrial
-              manufacturing; and travel, hospitality, & logistics
-            </li>
-          </ul>
-        </Grid>
-      </Grid>
-      <Grid>
-        <Tooltip title="Feature under construction" arrow placement="top">
-          <>
-            <FormControlLabel
-              label="Don't show this page again?"
-              control={<Checkbox disabled />}
-            />
+        <Grid container className="home-modal__footer">
+          <FormControlLabel
+            label="Don't show this page again?"
+            control={<Checkbox disabled checked />}
+          />
 
-            <Button className="button--primary" disabled>
-              Go to dashboard
-            </Button>
-          </>
-        </Tooltip>
-      </Grid>
+          <Button className="button--primary" disabled>
+            Go to dashboard
+          </Button>
+        </Grid>
+      </CustomScrollbar>
     </div>
   );
 };
--- src/screens/StaticHomeScreen/index.tsx
+++ src/screens/StaticHomeScreen/index.tsx
@@ -2,20 +2,26 @@ import Appbar from "../../components/Appbar";
 import CustomScrollbar from "../../components/CustomScrollbar";
 import StaticDashboard from "../../components/StaticDashboard";
 import dashboardImage from "../../assets/images/dashboard-chart.png";
-import { Modal, Paper } from "@material-ui/core";
-
+import { Dialog, Modal, Paper } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import { useState } from "react";
 const StaticHomeScreen: React.FC = () => {
+  const [openPopup, setOpenPopup] = useState<boolean>(true);
   return (
     <div className="home-screen">
       <Appbar variant="fullWidth" />
       <main className="home-screen__content">
         <CustomScrollbar>
           <img src={dashboardImage} alt="" />
-          <Modal open={true} className="home-modal">
-            <Paper elevation={4}>
-              <StaticDashboard />
-            </Paper>
-          </Modal>
+          <Dialog open={openPopup} className="home-modal">
+            <CloseIcon
+              className="home-modal__close-icon"
+              onClick={() => setOpenPopup(false)}
+            />
+            <StaticDashboard />
+            {/* <Paper elevation={4}>
+            </Paper> */}
+          </Dialog>
         </CustomScrollbar>
       </main>
     </div>
