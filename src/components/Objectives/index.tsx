62b24cde32e1ba34f983da8e66c68417554de887
Gaurav Jain <gaurav_jain@external.mckinsey.com>
2021-12-03 18:06:11 +0530

    scrollbar issue

--- src/components/Objectives/index.tsx
+++ src/components/Objectives/index.tsx
@@ -1,16 +1,84 @@
-import React from 'react'
+import React from "react";
 import "./Objectives.scss";
+import CustomScrollbar from "../CustomScrollbar";
+import { ReactComponent as ResearchObjective } from "../../assets/svg/research_objective.svg";
+import { ReactComponent as BusinessObjective } from "../../assets/svg/business_objective.svg";
+import { ReactComponent as HardrockLogo } from "../../assets/svg/hardrock_logo.svg";
 
-interface Props {
-    
-}
+interface Props {}
 
 const Objectives = (props: Props) => {
-    return (
-        <div>
-           Objectives
+  return (
+    <div className="objectives">
+      <div className="left-Area">
+        <h2>OBJECTIVES</h2>
+        <div className="leftContain">
+          <div>
+            <BusinessObjective />
+          </div>
+          <div className="left-line">
+            <p>
+              <b>Business Objective: </b>: Hard Rock Sportsbook recognizes the
+              need to build its foundational knowledge of the betting market and
+              a baseline reading of its brand health in Florida (to start) ahead
+              of its launch. Hard Rock Sportsbook is focused on :
+            </p>
+            <ul>
+              <li>
+                <p>Understanding its target audience</p>
+              </li>
+              <li>
+                <p>Increasing awareness</p>
+              </li>
+              <li>
+                <p>Customer acquisition</p>
+              </li>
+              <li>
+                <p>Market share for the hard rock sportsbook offering </p>
+              </li>
+            </ul>
+            <p>
+              This includes uncovering the nuances that come up with the Hard
+              Rock brand name and how that name might be helpful (or not) in
+              gaining credibility for Hard Rock Sportsbook
+            </p>
+          </div>
         </div>
-    )
-}
+        <div className="leftContain">
+          <div>
+            <ResearchObjective />
+          </div>
+          <div className="left-line">
+            <p>
+              <b>Research Objective: </b>There are two main research objectives
+              :
+            </p>
+            <ul>
+              <li>
+                <p>
+                  <b>Target Audience Deep Dive: </b>Profile the sports bettor in
+                  Florida – understand who they are, how they engage with sports
+                  betting (currently and in the future) and key barriers and
+                  drivers to sports betting (in general and with Hard Rock
+                  Sportsbook specifically). See how Hard Rock Sportsbook
+                  compares to key competitors
+                </p>
+              </li>
+              <li>
+                <p>
+                  <b>Brand Health: </b>Uncover current brand awareness and
+                  perceptions (and set a baseline for ongoing measurement)
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="rightContain">
+        <HardrockLogo />
+      </div>
+    </div>
+  );
+};
 
-export default Objectives
+export default Objectives;
--- src/components/StaticDashboard/index.tsx
+++ src/components/StaticDashboard/index.tsx
@@ -1,6 +1,6 @@
 import { Grid } from "@material-ui/core";
 import DoshboardChart from "../../assets/svg/statics-chart.svg";
-import Carousel from 'react-material-ui-carousel';
+import Carousel from "react-material-ui-carousel";
 import SlideFirst from "../../assets/svg/slide1.svg";
 import Slidesecond from "../../assets/svg/slide2.svg";
 import Objectives from "../Objectives";
@@ -13,11 +13,11 @@ const StaticDashboard: React.FC<StaticDashboardProps> = (props) => {
   return (
     <Carousel>
       <div>
-         <Objectives/>
+        <Objectives />
       </div>
-      <div>
+      {/* <div>
         <Methodology />
-      </div>
+      </div> */}
     </Carousel>
   );
 };
