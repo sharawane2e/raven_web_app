05e12358a1c3ce7c9603c95147b46c71f72f5253
ashishgangware2e <ashish.kumar@e2eresearch.com>
2022-02-16 17:36:16 +0530

    object screen update

--- src/components/Objectives/index.tsx
+++ src/components/Objectives/index.tsx
@@ -1,16 +1,15 @@
-import React from "react";
 import "./Objectives.scss";
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import { ReactComponent as ResearchObjective } from "../../assets/svg/research_objective.svg";
 import { ReactComponent as BusinessObjective } from "../../assets/svg/business_objective.svg";
 import { ReactComponent as SHFFinancial } from "../../assets/svg/SHF_Financial_Logo.svg";
 
-import { ReactComponent as OnlineSurveyIcon } from '../../assets/svg/online_survey.svg';
-import { ReactComponent as CalendarIcon } from '../../assets/svg/calendar_icon.svg';
-import { ReactComponent as TotalRBIcon } from '../../assets/svg/total_RB.svg';
-import { ReactComponent as PopulationIcon } from '../../assets/svg/population.svg';
-import { ReactComponent as FolridaMapIcon } from '../../assets/svg/florida_map.svg';
+import { ReactComponent as OnlineSurveyIcon } from "../../assets/svg/online_survey.svg";
+import { ReactComponent as CalendarIcon } from "../../assets/svg/calendar.svg";
+import { ReactComponent as TotalRBIcon } from "../../assets/svg/target.svg";
+import { ReactComponent as PopulationIcon } from "../../assets/svg/Industry_target.svg";
+import { ReactComponent as FolridaMapIcon } from "../../assets/svg/map.svg";
 
 interface Props {}
 
@@ -24,17 +23,23 @@ const Objectives = (props: Props) => {
             <BusinessObjective />
             <div className="objectives--content--text objectives--content--border">
               <p>
-                <b className="objectives--content--text objectives--content--text--blue">Business Objective: </b> In order to better meet the needs of existing and potential clients and
-                to successfully grow our business, it is important to understand the
-                perspectives of executive level financial officers at both public and private
-                institutions and corporations. Specifically, we need to:
+                <b className="objectives--content--text objectives--content--text--blue">
+                  Business Objective:
+                </b>
+                In order to better meet the needs of existing and potential
+                clients and to successfully grow our business, it is important
+                to understand the perspectives of executive level financial
+                officers at both public and private institutions and
+                corporations. Specifically, we need to:
               </p>
               <ul>
                 <li>
                   <p>Better understand our target audience</p>
                 </li>
                 <li>
-                  <p>Increase awareness of our brand among high value customer</p>
+                  <p>
+                    Increase awareness of our brand among high value customers
+                  </p>
                 </li>
                 <li>
                   <p>Acquire key high value customers</p>
@@ -50,102 +55,104 @@ const Objectives = (props: Props) => {
               </p> */}
             </div>
           </div>
-          
+
           <div className="objectives--content">
             <ResearchObjective />
             <div className="objectives--content--text objectives--content--border">
               <p>
-                <b className="objectives--content--text objectives--content--text--blue">Research Objective: </b>: Key objectives for this research include:
+                <b className="objectives--content--text objectives--content--text--blue">
+                  Research Objective:
+                </b>
+                : Key objectives for this research include:
               </p>
               <ul>
                 <li>
-                <p>Better understand our target audience
+                  <p>
+                    Understand how innovation and enterprise-wide performance
+                    management are viewed and planned for
                   </p>
                 </li>
                 <li>
-                <p>
-                Increase awareness of our brand among high value customers
+                  <p>
+                    Understand the type and extent of changes to performance
+                    management systems that are expected to contribute to
+                    business growth in terms of people, processes, and
+                    technologies
                   </p>
-                </li>
-
-                <li>
-                <p>
-                Acquire key high value customers
-                  </p>
-                </li>
-                <li>
-                <p>
-                Grow our market share in key geographic regions
-                </p>
                 </li>
               </ul>
             </div>
           </div>
-
         </div>
         <div className="objectives--container container--right">
           <SHFFinancial className="objectives--container container--right-svg" />
         </div>
         <div className="clearAll"></div>
-        
       </div>
-      <h2>OBJECTIVES</h2>
-        <div className="carousal-section">
-          <Grid container justifyContent="center" spacing={2}>
-            {/* {[0, 1, 2,3,4].map((value) => ( */}
-              <Grid item>
-                <Paper sx={{ height: 180, width: 20 }} >
-                    <div className="card-image">
-                    <OnlineSurveyIcon/>
-                    </div>
-                <div className="card-title">
-                    Online Survey
-                </div>
-                </Paper>
-              </Grid>
-              <Grid item>
-                <Paper sx={{ height: 180, width: 20 }} >
-                    <div className="card-image">
-                    <TotalRBIcon/>
-                    </div>
-                <div className="card-title">
-                Total = 1,000 Florida Residents = 800 Florida Visitors = 200
-                </div>
-                </Paper>
-              </Grid>
-              <Grid item>
-                <Paper sx={{ height: 180, width: 20 }} >
-                    <div className="card-image">
-                    <PopulationIcon/>
-                    </div>
-                <div className="card-title">
-                Population:Ages 21+
-                </div>
-                </Paper>
-              </Grid>
-              <Grid item>
-                <Paper sx={{ height: 180, width: 20 }} >
-                    <div className="card-image">
-                    <FolridaMapIcon/>
-                    </div>
-                <div className="card-title">
-                      Profile the sports bettor in Florida
-                </div>
-                </Paper>
-              </Grid>
-              <Grid item>
-                <Paper sx={{ height: 180, width: 20 }} >
-                    <div className="card-image">
-                    <CalendarIcon/>
-                    </div>
-                <div className="card-title">
-                Fielded:August 2021
-                </div>
-                </Paper>
-              </Grid>
-            {/* ))} */}
+
+      <div className="carousal-section">
+        <h2 className="h2">OBJECTIVES</h2>
+        <Grid container justifyContent="center" spacing={2}>
+          {/* {[0, 1, 2,3,4].map((value) => ( */}
+          <Grid item>
+            <Paper sx={{ height: 180, width: 20 }}>
+              <div className="card-image">
+                <OnlineSurveyIcon />
+              </div>
+              <div className="card-title">Online Survey</div>
+              <div className="card-title card-title-unbold">
+                LOI = 13 minutes
+              </div>
+            </Paper>
           </Grid>
-        </div>
+          <Grid item>
+            <Paper sx={{ height: 180, width: 20 }}>
+              <div className="card-image">
+                <TotalRBIcon />
+              </div>
+              <div className="card-title">Sample Size:</div>
+              <div className="card-title card-title-unbold">N= 500</div>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper sx={{ height: 180, width: 20 }}>
+              <div className="card-image">
+                <PopulationIcon />
+              </div>
+              <div className="card-title">Industry Target:</div>
+              <div className="card-title card-title-unbold">
+                CFO, VP of Finance
+              </div>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper sx={{ height: 180, width: 20 }}>
+              <div className="card-image">
+                <FolridaMapIcon />
+              </div>
+              <div className="card-title">Demographic Target</div>
+              <div className="card-title card-title-unbold">
+                Global, minimum
+              </div>
+              <div className="card-title card-title-unbold">
+                n= 25 per country
+              </div>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper sx={{ height: 180, width: 20 }}>
+              <div className="card-image">
+                <CalendarIcon />
+              </div>
+              <div className="card-title">Field Dates:</div>
+              <div className="card-title card-title-unbold">
+                Fielded:August 2021
+              </div>
+            </Paper>
+          </Grid>
+          {/* ))} */}
+        </Grid>
+      </div>
     </div>
   );
 };
