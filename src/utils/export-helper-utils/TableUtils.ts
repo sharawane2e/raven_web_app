2fa81d983db59e310fff79d4e0ba096f284441b8
ashishgangware2e <79965216+ashishgangware2e@users.noreply.github.com>
2022-04-07 19:21:03 +0530

    table util update

--- src/utils/export-helper-utils/TableUtils.ts
+++ src/utils/export-helper-utils/TableUtils.ts
@@ -1,11 +1,11 @@
-import { round } from "../Utility";
-import { chartDataGen } from "./ExportChartDataGen";
-import store from "../../redux/store";
-import { ChartLabelType } from "../../enums/ChartLabelType";
+import { round } from '../Utility';
+import { chartDataGen } from './ExportChartDataGen';
+import store from '../../redux/store';
+import { ChartLabelType } from '../../enums/ChartLabelType';
 
 export function tableChartDataGen() {
   let seriesData = [];
-   seriesData = chartDataGen();
+  seriesData = chartDataGen();
 
   let rows = [];
   let minmax = [];
@@ -13,53 +13,47 @@ export function tableChartDataGen() {
   const tranposedTableData: any[] = [];
   const tranposedTableDataMin: any[] = [];
 
-  const {chart} = store.getState();
+  const { chart } = store.getState();
 
   if (seriesData) {
     let scale: any = [];
     seriesData.forEach((index: any) => {
       scale.push(index.name);
     });
-    rows.push(["", ...scale, "Total"]);
+    rows.push(['', ...scale, 'Total']);
     let subRow: any = [];
-    let totalRow:any = [];    
-    let scaleIndex:any =0;
-    // console.log(seriesData[0])
-    let scaleLength:any=""
-    if(chart.questionData?.groupNetData){
+    let totalRow: any = [];
+    let scaleIndex: any = 0;
+    let scaleLength: any = '';
+
+    if (chart.questionData?.groupNetData) {
       scaleLength = chart.questionData?.groupNetData.length;
     }
-    console.log("scaleLength",scaleLength)
-    
-    
-    if(chart.chartTranspose){
-      scaleIndex = scale.length
-    }else{
-      scaleIndex = scale.length-scaleLength
+    if (chart.chartTranspose) {
+      scaleIndex = scale.length;
+    } else {
+      scaleIndex = scale.length - scaleLength;
     }
 
-   if(seriesData[0])
-      
+    if (seriesData[0])
       for (let k = 0; k < seriesData[0].labels.length; k++) {
-        let totalrowSub=0;
-        
-        
-        seriesData.forEach((d: any, rIndex:any) => {          
-          if(chart?.chartLabelType===ChartLabelType.PERCENTAGE){
-            if (d.values[k]) {
-              subRow.push(round(d.values[k], 1) + "%");
-              if(rIndex < scaleIndex){
-                 totalrowSub += parseFloat(d.values[k]);
-               }
+        let totalrowSub = 0;
 
+        seriesData.forEach((d: any, rIndex: any) => {
+          if (chart?.chartLabelType === ChartLabelType.PERCENTAGE) {
+            if (d.values[k]) {
+              subRow.push(round(d.values[k], 1) + '%');
+              if (rIndex < scaleIndex) {
+                totalrowSub += parseFloat(d.values[k]);
+              }
             } else {
-              subRow.push(0 + "%");
+              subRow.push(0 + '%');
               totalrowSub += 0;
             }
-          }else{
+          } else {
             if (d.values[k]) {
               subRow.push(round(d.values[k], 1));
-              if(rIndex < scaleIndex){
+              if (rIndex < scaleIndex) {
                 totalrowSub += parseFloat(d.values[k]);
               }
             } else {
@@ -67,77 +61,71 @@ export function tableChartDataGen() {
               totalrowSub += 0;
             }
           }
-          
-          
         });
-
-
-        
-        if(chart?.chartLabelType===ChartLabelType.PERCENTAGE){
-          totalRow.push(round(totalrowSub, 1) + "%");
-        }else{
+        if (chart?.chartLabelType === ChartLabelType.PERCENTAGE) {
+          totalRow.push(round(totalrowSub, 1) + '%');
+        } else {
           totalRow.push(round(totalrowSub, 1));
         }
-        
+
         rows.push([seriesData[0].labels[k], ...subRow, ...totalRow]);
-        
+
         minmax.push([tranposedTableData, tranposedTableDataMin]);
         subRow = [];
-        totalRow= [];
-        
+        totalRow = [];
       }
-}
-  let tColomn:any = [];
-  
-  seriesData.forEach((index: any) => {
+  }
+  let tColomn: any = [];
 
-    let getColumnSum=0;
-    let columnValues = index.values
-    
+  seriesData.forEach((series: any) => {
+    let getColumnSum = 0;
+    let columnValues = series.values;
+
     const updateRow: any[] = [];
     //let getColoumnTotal = columnValues.reduce((partialSum:any, a:any) => partialSum + a, 0);
-    for( var i = 0; i < columnValues.length; i++ ) {
-      if( typeof(columnValues[i])==="undefined" ) {
+    for (var i = 0; i < columnValues.length; i++) {
+      if (typeof columnValues[i] === 'undefined') {
         columnValues[i] = 0;
-        updateRow.push(columnValues[i])
+        updateRow.push(columnValues[i]);
+      } else {
+        updateRow.push(columnValues[i]);
       }
-      else{
-        updateRow.push(columnValues[i])
-      }
-     };
+    }
+    if (chart.chartTranspose) {
+      columnValues.splice(-4);
+    }
 
-     if(chart?.chartLabelType===ChartLabelType.PERCENTAGE){
+    if (chart?.chartLabelType === ChartLabelType.PERCENTAGE) {
+      tranposedTableData.push(Math.max(...updateRow) + '%');
+      tranposedTableDataMin.push(Math.min(...updateRow) + '%');
+    } else {
+      tranposedTableData.push(Math.max(...updateRow));
+      tranposedTableDataMin.push(Math.min(...updateRow));
+    }
 
-       tranposedTableData.push(Math.max(...updateRow)+'%');
-       tranposedTableDataMin.push(Math.min(...updateRow)+'%');
-      }else{
-       tranposedTableData.push(Math.max(...updateRow));
-       tranposedTableDataMin.push(Math.min(...updateRow));
+    let getColoumnTotal = series.values
+      .filter(function (x: any) {
+        return typeof x === 'number';
+      }) // remove any non numbers
+      .reduce(function (s: number, v: number) {
+        return s + Number(v);
+      }, 0);
 
-     }
-
-
-    let getColoumnTotal = index.values
-    .filter(function(x:any) { return typeof(x) === 'number'}) // remove any non numbers
-    .reduce(function( s:number, v:number ){ return s + Number(v) }, 0);
-    
-    if(chart?.chartLabelType===ChartLabelType.PERCENTAGE){
-      tColomn.push(round(getColoumnTotal, 1) + "%");
-    }else{
+    if (chart?.chartLabelType === ChartLabelType.PERCENTAGE) {
+      tColomn.push(round(getColoumnTotal, 1) + '%');
+    } else {
       tColomn.push(round(getColoumnTotal, 1));
     }
-  
-    getColumnSum=0;
-   
+
+    getColumnSum = 0;
   });
-  
+
   //console.log(tColomn);
 
   minmax.push([tranposedTableData, tranposedTableDataMin]);
 
-  rows.push(["Total", ...tColomn, ""]);
-  const complteTable={rows, minmax};
+  rows.push(['Total', ...tColomn, '']);
+  const complteTable = { rows, minmax };
 
   return complteTable;
 }
-
