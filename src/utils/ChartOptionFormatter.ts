273235dd158292030077f7fd975a18372c8d37ec
Gaurav Jain <gaurav.jain@e2eresearch.com>
2022-05-11 17:46:50 +0530

    adding standard and standard error context

--- src/utils/ChartOptionFormatter.ts
+++ src/utils/ChartOptionFormatter.ts
@@ -2,18 +2,19 @@ import {
   colorArr,
   decimalPrecision,
   primaryBarColor,
-} from '../constants/Variables';
-import { ChartLabelType } from '../enums/ChartLabelType';
-import { ChartType } from '../enums/ChartType';
-import { QuestionType } from '../enums/QuestionType';
-import { dataLabels } from '../redux/reducers/chartReducer';
-import store from '../redux/store';
-import { IQuestionOption } from '../types/IBaseQuestion';
-import { IQuestion } from '../types/IQuestion';
-import { getMatchedfilter, getmatchedFind, round } from './Utility';
-import _, { find, omit } from 'lodash';
-import { ChartOrientation } from '../enums/ChartOrientation';
-import { getNumberChartOption } from '../services/ChartNumberService';
+} from "../constants/Variables";
+import { ChartLabelType } from "../enums/ChartLabelType";
+import { ChartType } from "../enums/ChartType";
+import { QuestionType } from "../enums/QuestionType";
+import { dataLabels } from "../redux/reducers/chartReducer";
+import store from "../redux/store";
+import { IQuestionOption } from "../types/IBaseQuestion";
+import { IQuestion } from "../types/IQuestion";
+import { getMatchedfilter, getmatchedFind, round } from "./Utility";
+import _, { find, omit } from "lodash";
+import { ChartOrientation } from "../enums/ChartOrientation";
+import { getNumberChartOption } from "../services/ChartNumberService";
+import { OptionUnstyled } from "@mui/base";
 
 export const getChartOptions = (
   questionData: IQuestion | null = store.getState().chart.questionData,
@@ -22,7 +23,7 @@ export const getChartOptions = (
   bannerQuestionData: IQuestion | null = store.getState().chart
     .bannerQuestionData,
   chartOptionsData: any = store.getState().chart.chartOptions,
-  transposed: boolean = store.getState().chart.chartTranspose,
+  transposed: boolean = store.getState().chart.chartTranspose
 ): any => {
   if (questionData !== null) {
     switch (questionData.type) {
@@ -32,7 +33,7 @@ export const getChartOptions = (
           chartData,
           baseCount,
           bannerQuestionData,
-          chartOptionsData,
+          chartOptionsData
         );
       case QuestionType.MULTI:
         return getMultiChartOptions(
@@ -40,14 +41,14 @@ export const getChartOptions = (
           chartData,
           baseCount,
           bannerQuestionData,
-          chartOptionsData,
+          chartOptionsData
         );
       case QuestionType.RANK:
         return getRankChartOptions(
           questionData,
           chartData,
           baseCount,
-          transposed,
+          transposed
         );
       case QuestionType.GRID:
         return getGridChartOptions(questionData, chartData, baseCount);
@@ -60,7 +61,7 @@ export const getChartOptions = (
           baseCount,
           bannerQuestionData,
           chartOptionsData,
-          transposed,
+          transposed
         );
 
       default:
@@ -76,7 +77,7 @@ const getMultiChartOptions = (
   chartData: any[],
   baseCount: number,
   bannerQuestionData: IQuestion | null,
-  chartOptionsData: any,
+  chartOptionsData: any
 ): any => {
   const {
     questions: { bannerQuestionList },
@@ -130,12 +131,12 @@ const getMultiChartOptions = (
         if (optionData) {
           const label = optionData.find(
             // @ts-ignore
-            (option: any) => option.labelCode === bannerQuesOption.labelCode,
+            (option: any) => option.labelCode === bannerQuesOption.labelCode
           );
 
           let localBase = optionData?.reduce(
             (sum: number, option: any) => sum + option.count,
-            0,
+            0
           );
 
           const bannerQuestion: any = find(bannerQuestionList, function (o) {
@@ -197,7 +198,7 @@ const getMultiChartOptions = (
       const option = questionData.options[optionIndex];
       const label = chartData.find(
         (record: { labelCode: string; count: number }) =>
-          record.labelCode === option.labelCode,
+          record.labelCode === option.labelCode
       );
       let count = 0;
       if (label) {
@@ -218,7 +219,7 @@ const getMultiChartOptions = (
         const seriesObject = _.find(questionData.options, function (o) {
           return o.labelCode === option.labelCode;
         });
-        if (seriesObject?.labelCode.split('_')[0] == 'N') {
+        if (seriesObject?.labelCode.split("_")[0] == "N") {
           data.push({
             name: option.labelText,
             // y: round(plotValue, decimalPrecision),
@@ -226,7 +227,7 @@ const getMultiChartOptions = (
             percentageValue,
             numberValue,
             baseCount: baseCount,
-            color: '#01274c',
+            color: "#01274c",
           });
         } else {
           data.push({
@@ -283,7 +284,7 @@ const getSingleChartOptions = (
   chartData: any[],
   baseCount: number,
   bannerQuestionData: IQuestion | null,
-  chartOptionsData: any,
+  chartOptionsData: any
 ): any => {
   //debugger;
 
@@ -335,12 +336,12 @@ const getSingleChartOptions = (
         if (optionData) {
           const label = optionData.find(
             // @ts-ignore
-            (option: any) => option.labelCode === bannerQuesOption.labelCode,
+            (option: any) => option.labelCode === bannerQuesOption.labelCode
           );
 
           let localBase = optionData?.reduce(
             (sum: number, option: any) => sum + option.count,
-            0,
+            0
           );
 
           if (bannerQuestionData?.type == QuestionType.MULTI) {
@@ -403,7 +404,7 @@ const getSingleChartOptions = (
       const option = questionData.options[optionIndex];
       const label = chartData.find(
         (record: { labelCode: string; count: number }) =>
-          record.labelCode === option.labelCode,
+          record.labelCode === option.labelCode
       );
       let count = 0;
       if (label) {
@@ -468,7 +469,7 @@ const getRankChartOptions = (
   questionData: IQuestion,
   chartData: any,
   baseCount: number,
-  transposed: boolean,
+  transposed: boolean
 ): any => {
   // debugger;
   const categories = [];
@@ -476,7 +477,7 @@ const getRankChartOptions = (
 
   const subGroups = questionData.subGroups.filter((subGroup: any) => {
     const subGroupData = chartData.find(
-      (data: any) => data._id === subGroup.qId,
+      (data: any) => data._id === subGroup.qId
     );
     if (subGroupData && subGroupData.options.length) return true;
     return false;
@@ -509,7 +510,7 @@ const getRankChartOptions = (
 
       if (optionData) {
         label = optionData.options.find(
-          (option: any) => option.option === scale.labelCode,
+          (option: any) => option.option === scale.labelCode
         );
 
         if (label) {
@@ -531,7 +532,7 @@ const getRankChartOptions = (
             option: scale.labelCode,
           });
           if (chartOptionObject.length) {
-            newBaseCount = newBaseCount + chartOptionObject[0]['count'];
+            newBaseCount = newBaseCount + chartOptionObject[0]["count"];
           }
         });
       }
@@ -587,13 +588,13 @@ const getRankChartOptions = (
 const getGridChartOptions = (
   questionData: IQuestion,
   chartData: any,
-  baseCount: number,
+  baseCount: number
 ): any => {
   const categories = [];
   const series = [];
 
   const subGroups = questionData.subGroups.filter((subGroup: any) => {
-    const subGroupData = getmatchedFind(chartData, '_id', subGroup.qId);
+    const subGroupData = getmatchedFind(chartData, "_id", subGroup.qId);
     if (subGroupData && subGroupData.options.length) return true;
     return false;
   });
@@ -623,12 +624,12 @@ const getGridChartOptions = (
       const subGroup = subGroups[subGroupIndex];
       categories.push(subGroup.labelText);
 
-      const optionData = getmatchedFind(chartData, '_id', subGroup.qId);
+      const optionData = getmatchedFind(chartData, "_id", subGroup.qId);
 
       const labels = getMatchedfilter(
         optionData.options,
-        'option',
-        scale.labelCode,
+        "option",
+        scale.labelCode
       );
       const count = _.sumBy(labels, function (o) {
         return o.count;
@@ -686,13 +687,16 @@ const getGridChartOptions = (
 const getGridMeanChartOptions = (
   questionData: IQuestion,
   chartData: any,
-  baseCount: number,
+  baseCount: number
 ): any => {
+  // debugger;
   const {
     chart: { chartType },
   } = store.getState();
 
   const data: any[] = [];
+  const standardDeviation: any[] = [];
+  const standardError: any[] = [];
   // debugger;
   for (
     let optionIndex = 0;
@@ -700,12 +704,12 @@ const getGridMeanChartOptions = (
     optionIndex++
   ) {
     const option = questionData.subGroups[optionIndex];
-    const optionData = getmatchedFind(chartData, '_id', option.qId);
+    const optionData = getmatchedFind(chartData, "_id", option.qId);
     const filteredOptions = _.remove(
       [...optionData.options],
       function (n: any) {
         return !Array.isArray(n.option);
-      },
+      }
     ); //removing array options which come with subgroups
 
     const totalSelections = _.sumBy(filteredOptions, function (o: any) {
@@ -713,38 +717,62 @@ const getGridMeanChartOptions = (
     });
     const plotValue = totalSelections / baseCount;
 
-    if (plotValue > 0)
+    if (plotValue > 0) {
       data.push({
         name: option.labelText,
         // y: round(plotValue, decimalPrecision),
         y: plotValue,
         baseCount: baseCount,
       });
+    }
+
+    standardDeviation.push({
+      name: option.labelText,
+      y: 5,
+      baseCount: baseCount,
+    });
+
+    standardError.push({
+      name: option.labelText,
+      y: 10,
+      baseCount: baseCount,
+    });
   }
 
   const series: any[] = [];
+  const seriesLabels = ["Mean", "Standard Deviation", "Standard Error"];
+  const seriesData = [data, standardDeviation, standardError];
 
-  if (chartType === ChartType.STACK) {
-    data.map((element: any, index: number) => {
-      const name = element.name;
-      const color = colorArr[index];
-      const data = [
-        {
-          name: questionData?.labelText,
-          y: element.y,
-          baseCount: element.baseCount,
-        },
-      ];
-      series.push({ name, color, data, dataLabels });
-    });
-  } else {
+  for (let i = 0; i < 3; i++) {
     series.push({
-      color: primaryBarColor,
-      name: questionData?.labelText,
-      data,
+      color: colorArr[i],
+      name: seriesLabels[i],
+      data: seriesData[i],
       dataLabels,
     });
   }
+
+  // if (chartType === ChartType.STACK) {
+  //   data.map((element: any, index: number) => {
+  //     const name = element.name;
+  //     const color = colorArr[index];
+  //     const data = [
+  //       {
+  //         name: questionData?.labelText,
+  //         y: element.y,
+  //         baseCount: element.baseCount,
+  //       },
+  //     ];
+  //     series.push({ name, color, data, dataLabels });
+  //   });
+  // } else {
+  //   series.push({
+  //     color: primaryBarColor,
+  //     name: questionData?.labelText,
+  //     data,
+  //     dataLabels,
+  //   });
+  // }
 
   return {
     legend: {
@@ -759,14 +787,14 @@ const getGridMeanChartOptions = (
 const getGridMultiChartOptions = (
   questionData: IQuestion,
   chartData: any,
-  baseCount: number,
+  baseCount: number
 ): any => {
   const categories = [];
   const series = [];
 
   const subGroups = questionData.subGroups.filter((subGroup: any) => {
     const subGroupData = chartData.find(
-      (data: any) => data._id === subGroup.qId,
+      (data: any) => data._id === subGroup.qId
     );
     if (subGroupData && subGroupData.options.length) return true;
     return false;
@@ -796,7 +824,7 @@ const getGridMultiChartOptions = (
       let label;
       if (optionData) {
         label = optionData.options.find(
-          (option: any) => option.option === scale.labelCode,
+          (option: any) => option.option === scale.labelCode
         );
 
         if (label) {
@@ -864,23 +892,23 @@ export const getToolTip = () => {
     chart: { questionData, showMean },
   } = store.getState();
   const tooltip: { headerFormat: String; pointFormat: String } = {
-    headerFormat: '',
-    pointFormat: '',
+    headerFormat: "",
+    pointFormat: "",
   };
 
-  tooltip['headerFormat'] =
+  tooltip["headerFormat"] =
     '<span style="font-size:11px">{series.name}</span><br>';
 
   if (showMean) {
-    tooltip['pointFormat'] =
-      '<span>{point.name}</span>: Mean<b> {point.y:.2f}</b>,  of total <b>{point.baseCount}</b><br/>';
+    tooltip["pointFormat"] =
+      "<span>{point.name}</span>: Mean<b> {point.y:.2f}</b>,  of total <b>{point.baseCount}</b><br/>";
   } else {
     if (questionData?.type === QuestionType?.NUMBER) {
-      tooltip['pointFormat'] =
-        '<span>{point.name}</span>: Mean<b> {point.y:.2f}</b>,  of total <b>{point.baseCount}</b><br/>';
+      tooltip["pointFormat"] =
+        "<span>{point.name}</span>: Mean<b> {point.y:.2f}</b>,  of total <b>{point.baseCount}</b><br/>";
     } else {
-      tooltip['pointFormat'] =
-        '<span>{point.name}</span>: Count<b> {point.numberValue}, {point.percentageValue:.2f}%</b> of total <b>{point.baseCount}</b><br/>';
+      tooltip["pointFormat"] =
+        "<span>{point.name}</span>: Count<b> {point.numberValue}, {point.percentageValue:.2f}%</b> of total <b>{point.baseCount}</b><br/>";
     }
   }
 
@@ -888,66 +916,66 @@ export const getToolTip = () => {
 };
 
 export const getPlotOptions = (
-  chartType = store.getState().chart.chartType,
+  chartType = store.getState().chart.chartType
 ) => {
   const chartDataClone = JSON.parse(JSON.stringify(store.getState().chart));
-  let plotOptions = chartDataClone.chartOptions['plotOptions'];
-  plotOptions = omit(plotOptions, ['column', 'bar', 'pie', 'line']);
+  let plotOptions = chartDataClone.chartOptions["plotOptions"];
+  plotOptions = omit(plotOptions, ["column", "bar", "pie", "line"]);
   if (chartType === ChartType.STACK) {
-    plotOptions['column'] = {
-      stacking: 'normal',
+    plotOptions["column"] = {
+      stacking: "normal",
     };
 
-    plotOptions['series'].dataLabels.format = chartDataClone.showMean
-      ? '{point.y:.1f}'
+    plotOptions["series"].dataLabels.format = chartDataClone.showMean
+      ? "{point.y:.1f}"
       : chartDataClone.chartLabelType === ChartLabelType.PERCENTAGE
-      ? '{point.y:.1f}%'
-      : '{point.y:.0f}';
+      ? "{point.y:.1f}%"
+      : "{point.y:.0f}";
 
-    plotOptions['series'].dataLabels.y = undefined;
-    plotOptions['series'].dataLabels.rotation = 0;
+    plotOptions["series"].dataLabels.y = undefined;
+    plotOptions["series"].dataLabels.rotation = 0;
   } else if (chartType === ChartType.COLUMN) {
-    plotOptions['bar'] = {
-      stacking: 'normal',
+    plotOptions["bar"] = {
+      stacking: "normal",
     };
-    plotOptions['series'].dataLabels.format = chartDataClone.showMean
-      ? '{point.y:.1f}'
+    plotOptions["series"].dataLabels.format = chartDataClone.showMean
+      ? "{point.y:.1f}"
       : chartDataClone.chartLabelType === ChartLabelType.PERCENTAGE
-      ? '{point.y:.1f}%'
-      : '{point.y:.0f}';
+      ? "{point.y:.1f}%"
+      : "{point.y:.0f}";
     if (chartDataClone.chartOrientation === ChartOrientation.PORTRAIT) {
-      plotOptions['series'].dataLabels.y = -20;
-      plotOptions['series'].dataLabels.rotation = -90;
+      plotOptions["series"].dataLabels.y = -20;
+      plotOptions["series"].dataLabels.rotation = -90;
     } else {
-      plotOptions['series'].dataLabels.y = undefined;
-      plotOptions['series'].dataLabels.rotation = 0;
+      plotOptions["series"].dataLabels.y = undefined;
+      plotOptions["series"].dataLabels.rotation = 0;
     }
   } else if (chartType === ChartType.PIE) {
-    plotOptions['pie'] = {
+    plotOptions["pie"] = {
       allowPointSelect: false,
-      cursor: 'pointer',
+      cursor: "pointer",
     };
-    plotOptions['series'].dataLabels.format = chartDataClone.showMean
-      ? '<b>{point.name}</b>: {point.y:.1f}'
+    plotOptions["series"].dataLabels.format = chartDataClone.showMean
+      ? "<b>{point.name}</b>: {point.y:.1f}"
       : chartDataClone.chartLabelType === ChartLabelType.PERCENTAGE
-      ? '<b>{point.name}</b>: {point.percentage:.1f}%'
-      : '<b>{point.name}</b>: {point.y:.0f}';
-    delete plotOptions['series'].dataLabels.y;
-    delete plotOptions['series'].dataLabels.rotation;
+      ? "<b>{point.name}</b>: {point.percentage:.1f}%"
+      : "<b>{point.name}</b>: {point.y:.0f}";
+    delete plotOptions["series"].dataLabels.y;
+    delete plotOptions["series"].dataLabels.rotation;
   } else if (chartType === ChartType.LINE) {
-    plotOptions['line'] = {
+    plotOptions["line"] = {
       // allowPointSelect: false,
       // cursor: "pointer",
     };
 
-    plotOptions['series'].dataLabels.format = chartDataClone.showMean
-      ? '{point.y:.1f}'
+    plotOptions["series"].dataLabels.format = chartDataClone.showMean
+      ? "{point.y:.1f}"
       : chartDataClone.chartLabelType === ChartLabelType.PERCENTAGE
-      ? '{point.y:.1f}%'
-      : '{point.y:.0f}';
+      ? "{point.y:.1f}%"
+      : "{point.y:.0f}";
   } else {
-    delete plotOptions['series'].dataLabels.y;
-    delete plotOptions['series'].dataLabels.rotation;
+    delete plotOptions["series"].dataLabels.y;
+    delete plotOptions["series"].dataLabels.rotation;
   }
   return plotOptions;
 };
