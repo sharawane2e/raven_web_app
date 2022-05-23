38a6dd7099d443edb91d15c29711d50c685ddecf
ashishgangware2e <79965216+ashishgangware2e@users.noreply.github.com>
2022-05-23 17:22:20 +0530

    standard deviation with banner

--- src/services/ChartService.ts
+++ src/services/ChartService.ts
@@ -388,6 +388,7 @@ export const transposeChart = () => {
     (chartDataClone.questionData.type == QuestionType.SINGLE ||
       chartDataClone.questionData.type == QuestionType.MULTI)
   ) {
+    // console.log('transpose data for update');
     const { chartData } = chartDataClone;
     const allLabels: Array<string> = [];
     const newChartData: any = {};
