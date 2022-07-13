f986350b77c97d89b772af0ea1484abc53c0e57f
ashishgangware2e <ashish.kumar@e2eresearch.com>
2022-07-14 01:42:28 +0530

    single to multi table update

--- src/utils/export-helper-utils/BannerQuesUtils.ts
+++ src/utils/export-helper-utils/BannerQuesUtils.ts
@@ -1,23 +1,24 @@
-import { decimalPrecision } from '../../constants/Variables';
-import { IBaseQuestion, IQuestionOption } from '../../types/IBaseQuestion';
+import { decimalPrecision } from "../../constants/Variables";
+import { IBaseQuestion, IQuestionOption } from "../../types/IBaseQuestion";
 import {
   getMatchedfilter,
   getmatchedFind,
   indexToChar,
   round,
   significantDifference,
-} from '../Utility';
-import store from '../../redux/store';
-import { ChartLabelType } from '../../enums/ChartLabelType';
-import _, { find } from 'lodash';
-import { QuestionType } from '../../enums/QuestionType';
-import { SignificantObject } from '../chart-option-util/single';
+} from "../Utility";
+import store from "../../redux/store";
+import { ChartLabelType } from "../../enums/ChartLabelType";
+import _, { find } from "lodash";
+import { QuestionType } from "../../enums/QuestionType";
+import { SignificantObject } from "../chart-option-util/single";
 
 export function bannerChartDataGen(
   questionData: IBaseQuestion,
   chartData: any,
   bannerQuestionData: any,
   chartTranspose: any,
+  questionChartData: any
 ) {
   const {
     chart: { chartLabelType },
@@ -33,24 +34,53 @@ export function bannerChartDataGen(
     if (!chartTranspose && bannerQuestionData?.type == QuestionType.SINGLE) {
       seriesData.length = 0;
       seriesData.push(
-        ...getSingleOptions(bannerQuestionData, questionData, chartData),
+        ...getSingleOptions(
+          bannerQuestionData,
+          questionData,
+          chartData,
+          questionChartData
+        )
       );
     } else {
+      console.log("elsee");
       seriesData.length = 0;
       seriesData.push(
         ...getSingleTransposeTableOptions(
           questionData,
           chartData,
           bannerQuestionData,
-          chartTranspose,
-        ),
+          chartTranspose
+        )
       );
     }
   } else {
-    seriesData.length = 0;
-    seriesData.push(
-      ...getSingleOptions(bannerQuestionData, questionData, chartData),
-    );
+    if (chartTranspose) {
+      seriesData.length = 0;
+      seriesData.push(
+        ...getMultiTransposeTableOptions(
+          questionData,
+          chartData,
+          bannerQuestionData
+        )
+      );
+      // console.log(
+      //   getMultiTransposeTableOptions(
+      //     questionData,
+      //     chartData,
+      //     bannerQuestionData
+      //   )
+      // );
+    } else {
+      seriesData.length = 0;
+      seriesData.push(
+        ...getSingleOptions(
+          bannerQuestionData,
+          questionData,
+          chartData,
+          questionChartData
+        )
+      );
+    }
   }
 
   return seriesData;
@@ -60,6 +90,7 @@ const getSingleOptions = (
   bannerQuestionData: any,
   questionData: any,
   chartData: any,
+  questionChartData: any
 ) => {
   const {
     chart: { chartLabelType, significant },
@@ -113,13 +144,17 @@ const getSingleOptions = (
                 }
               }
               optionData = newOptionData;
-              labelCodeSum.forEach((el: any) => {
-                const localbaseCount = el?.reduce(
-                  (sum: number, option: any) => sum + option.count,
-                  0,
-                );
-                baseCountSum.push(localbaseCount);
-              });
+              if (bannerQuestionData?.type == QuestionType.MULTI) {
+              } else {
+                labelCodeSum.forEach((el: any) => {
+                  const localbaseCount = el?.reduce(
+                    (sum: number, option: any) => sum + option.count,
+                    0
+                  );
+                  baseCountSum.push(localbaseCount);
+                });
+              }
+
               count = labeCodeSum;
               const sumofValue = _.sum(baseCountSum);
               localBase = sumofValue;
@@ -127,15 +162,15 @@ const getSingleOptions = (
               optionData = chartData[0][option.labelCode];
               const label = getMatchedfilter(
                 optionData,
-                'labelCode',
-                scaleOption.labelCode,
+                "labelCode",
+                scaleOption.labelCode
               );
               count = _.sumBy(label, function (o) {
                 return o.count;
               });
               localBase = optionData?.reduce(
                 (sum: number, option: any) => sum + option.count,
-                0,
+                0
               );
             }
 
@@ -147,7 +182,7 @@ const getSingleOptions = (
           } else {
             subOptionDataPercentage = round(
               (count / localBase) * 100,
-              decimalPrecision,
+              decimalPrecision
             );
             subOptionDataCount = count;
           }
@@ -162,38 +197,50 @@ const getSingleOptions = (
           const obj = chartDataComplete[option.labelCode] || [];
 
           if (obj && obj.length != 0) {
-            let base = obj?.reduce(
-              (sum: number, option: any) => sum + option.count,
-              0,
-            );
+            let base: any;
+            //  base = obj?.reduce(
+            //   (sum: number, option: any) => sum + option.count,
+            //   0
+            // );
+
+            if (bannerQuestionData?.type == QuestionType.MULTI) {
+              base = find(questionChartData, {
+                labelCode: option.labelCode,
+              })?.count;
+            } else {
+              base = obj?.reduce(
+                (sum: number, option: any) => sum + option.count,
+                0
+              );
+            }
 
             if (
               bannerQuestionData.type == QuestionType.MULTI &&
               questionData.type == QuestionType.MULTI
             ) {
-              base = find(chartData[1], function (o) {
-                return o.labelCode === option.labelCode;
-              })?.count;
+              // base = find(chartData[1], function (o) {
+              //   return o.labelCode === option.labelCode;
+              // })?.count;
             }
             if (
               bannerQuestionData.type == QuestionType.MULTI &&
               questionData.type == QuestionType.SINGLE
             ) {
-              const optionData = chartData[0][option?.labelCode];
-
-              base = optionData?.reduce(
-                (sum: number, option: any) => sum + option.count,
-                0,
-              );
+              // const optionData = chartData[0][option?.labelCode];
+              // base = optionData?.reduce(
+              //   (sum: number, option: any) => sum + option.count,
+              //   0
+              // );
             }
+            //   console.log("base", base);
 
             if (
               bannerQuestionData.type == QuestionType.SINGLE &&
               questionData.type == QuestionType.MULTI
             ) {
-              base = _.sumBy(chartData[0][option.labelCode], function (o: any) {
-                return o.count;
-              });
+              // base = _.sumBy(chartData[0][option.labelCode], function (o: any) {
+              //   return o.count;
+              // });
             }
             let subOptionData;
             subOptionData = obj.find((subObj: any) => {
@@ -210,7 +257,7 @@ const getSingleOptions = (
             if (subOptionData !== undefined) {
               subOptionDataPercentage = round(
                 (subOptionData.count / base) * 100,
-                decimalPrecision,
+                decimalPrecision
               );
               subOptionDataCount = subOptionData.count;
             } else {
@@ -244,11 +291,92 @@ const getSingleOptions = (
   return seriesData;
 };
 
+const getMultiTransposeTableOptions = (
+  questionData: any,
+  chartData: any,
+  bannerQuestionData: any
+) => {
+  //console.log("questionData", bannerQuestionData);
+  const {
+    chart: { chartLabelType },
+  } = store.getState();
+  const seriesData: any[] = [];
+  const labelCodeArr: string[] = [];
+  const baseCountArr: number[] = [];
+
+  const labels: any = [];
+  for (const singleSeriesArr in chartData[0]) {
+    chartData[0][singleSeriesArr].forEach((serieObject: any) => {
+      if (labelCodeArr.indexOf(serieObject.labelCode) == -1) {
+        labelCodeArr.push(serieObject.labelCode);
+        baseCountArr.push(0);
+      }
+    });
+  }
+  labelCodeArr.forEach((labelCode: string, labelCodeIndex: number) => {
+    for (const singleSeriesArr in chartData[0]) {
+      const serieObject: any = getMatchedfilter(
+        chartData[0][singleSeriesArr],
+        "labelCode",
+        labelCode
+      );
+      baseCountArr[labelCodeIndex] += serieObject[0]?.count
+        ? serieObject[0]?.count
+        : 0;
+    }
+  });
+
+  bannerQuestionData.options.forEach((bannerData: any) => {
+    labels.push(bannerData.labelText);
+  });
+
+  questionData.options.forEach(
+    (questionOption: any, questionOptionIndex: number) => {
+      const baseCounts: any = [];
+      const percentageValues: any = [];
+      const countValues: any = [];
+      const name = questionOption.labelText;
+      bannerQuestionData.options.forEach(
+        (bannerOption: any, bannerOptionIndex: number) => {
+          const chartDataArr = chartData[0][questionOption.labelCode];
+          const chartObjectArr: any = getMatchedfilter(
+            chartDataArr,
+            "labelCode",
+            bannerOption.labelCode
+          );
+          const numberValue = chartObjectArr[0]?.count
+            ? chartObjectArr[0]?.count
+            : 0;
+
+          const baseCountIndex = labelCodeArr.indexOf(bannerOption.labelCode);
+          const baseCount = baseCountArr[baseCountIndex];
+          const percentageValue = (numberValue / baseCount) * 100;
+          percentageValues.push(percentageValue);
+          baseCounts.push(baseCount);
+          countValues.push(numberValue);
+        }
+      );
+
+      seriesData.push({
+        name,
+        labels,
+        values:
+          chartLabelType === ChartLabelType.PERCENTAGE
+            ? percentageValues
+            : countValues,
+        percentageValues,
+        baseCounts,
+      });
+    }
+  );
+  return seriesData;
+};
+
 const getSingleTransposeTableOptions = (
   questiondata: any,
   chartData: any,
   bannerQuestionData: any,
-  chartTranspose: any,
+  chartTranspose: any
 ) => {
   const {
     chart: { chartLabelType, significant },
@@ -258,7 +386,7 @@ const getSingleTransposeTableOptions = (
   let localBase = 0;
   const newOptionData: any = [];
   const labels: Array<string> = bannerQuestionData.options.map(
-    (label: IQuestionOption) => label.labelText,
+    (label: IQuestionOption) => label.labelText
   );
 
   const allLabels: Array<string> = [];
@@ -317,8 +445,8 @@ const getSingleTransposeTableOptions = (
 
             const label = getmatchedFind(
               optionData,
-              'labelCode',
-              option?.labelCode,
+              "labelCode",
+              option?.labelCode
             );
 
             count = label?.count;
@@ -334,7 +462,7 @@ const getSingleTransposeTableOptions = (
           } else {
             subOptionDataPercentage = round(
               (count / localBase) * 100,
-              decimalPrecision,
+              decimalPrecision
             );
             subOptionDataCount = count;
           }
@@ -342,7 +470,7 @@ const getSingleTransposeTableOptions = (
           percentageValues.push(subOptionDataPercentage);
           baseCounts.push(localBase);
         }
-      },
+      }
     );
     seriesData.push({
       name: scaleOption.labelText,
@@ -366,16 +494,16 @@ const getSingleTransposeTableOptions = (
 const getTablesignificantdifference = (seriesData: any) => {
   for (let i = 0; i < seriesData.length; i++) {
     const seriesupdatedLabels = [];
-    seriesData[i]['significance'] = [];
-    seriesData[i]['significanceDifference'] = [];
+    seriesData[i]["significance"] = [];
+    seriesData[i]["significanceDifference"] = [];
 
-    for (let j = 0; j < seriesData[i]['labels'].length; j++) {
-      seriesData[i]['significance'].push(indexToChar(j));
+    for (let j = 0; j < seriesData[i]["labels"].length; j++) {
+      seriesData[i]["significance"].push(indexToChar(j));
       seriesupdatedLabels.push(
-        seriesData[i]['labels'][j] + `(${indexToChar(j)})`,
+        seriesData[i]["labels"][j] + `(${indexToChar(j)})`
       );
     }
-    seriesData[i]['labels'] = seriesupdatedLabels;
+    seriesData[i]["labels"] = seriesupdatedLabels;
   }
   for (let i = 0; i < seriesData.length; i++) {
     for (let j = 0; j < seriesData[i].percentageValues.length; j++) {
@@ -394,7 +522,7 @@ const getTablesignificantdifference = (seriesData: any) => {
 
           const isSignificant = significantDifference(
             SignificantObject1,
-            SignificantObject2,
+            SignificantObject2
           );
 
           if (isSignificant) {
@@ -404,8 +532,8 @@ const getTablesignificantdifference = (seriesData: any) => {
       }
 
       if (significantArry.length) {
-        seriesData[i]['significanceDifference'][j] =
-          '(' + significantArry.join('') + ')';
+        seriesData[i]["significanceDifference"][j] =
+          "(" + significantArry.join("") + ")";
       }
     }
   }
