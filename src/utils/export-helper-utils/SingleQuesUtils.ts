6277e570021d39ed39a027d566a2928c4b24faec
ashishgangware2e <79965216+ashishgangware2e@users.noreply.github.com>
2022-07-26 17:13:45 +0530

    single Question util

--- src/utils/export-helper-utils/SingleQuesUtils.ts
+++ src/utils/export-helper-utils/SingleQuesUtils.ts
@@ -6,31 +6,45 @@ export function singleChartDataGen(series: any) {
   let values: any = [];
   let seriesData: any[] = [];
 
-  console.log(series.length);
-
   if (series.length == 1) {
     labelName.push(series[0].name);
     series[0].data.forEach((data: any) => {
       labels.push(data.name);
       values.push(data.y);
     });
+    seriesData = [
+      {
+        name: labelName[0],
+        labels,
+        values,
+      },
+    ];
+    return seriesData;
   } else {
-    // console.log('series', series);
+    const stackSeriesData: any = [];
+    const labelName: any = [];
     series?.forEach((seriesdata: any) => {
-      labels.push(seriesdata.name);
-      seriesdata.data.forEach((data: any) => {
-        labelName.push(data.name);
-        values.push(data.y);
+      labelName.push(seriesdata.name);
+      seriesdata.data((datael: any, index: number) => {
+        console.log('seriesdata', seriesdata.data);
+        //   stackSeriesData.push({
+        //     name: labelName[index],
+        //     labels: [seriesdata.name],
+        //     values: [datael.values],
+        //   });
       });
     });
+    // console.log(series);
+    // // console.log('series', series);
+    // series?.forEach((seriesdata: any) => {
+    //   labels.push(seriesdata.name);
+    //   seriesdata.data.forEach((data: any) => {
+    //     labelName.push(data.name);
+    //     values.push(data.y);
+    //   });
+    // });
+    return stackSeriesData;
   }
 
-  seriesData = [
-    {
-      name: labelName[0],
-      labels,
-      values,
-    },
-  ];
-  return seriesData;
+  //return seriesData;
 }
