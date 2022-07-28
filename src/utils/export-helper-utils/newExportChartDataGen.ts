61a4d36dcd7bd6d236cebebdbce4e80e4cb24517
Gaurav Jain <gaurav.jain@e2eresearch.com>
2022-07-29 04:10:50 +0530

    update for multiQExport

--- src/utils/export-helper-utils/newExportChartDataGen.ts
+++ src/utils/export-helper-utils/newExportChartDataGen.ts
@@ -3,59 +3,7 @@ import { MultiQuesExportUtils } from "./MultiQuesExportUtils";
 export function newChartDataGen(newSeriesData: any) {
   let seriesData: any[] = [];
 
-  //   const {
-  //     chartData,
-  //     questionData,
-  //     bannerQuestionData,
-  //     baseCount,
-  //     chartTranspose,
-  //     questionChartData,
-  //     chartOptions,
-  //     chartLabelType,
-  //   } = chart;
-
-  // const { selectedBannerQuestionId } = questions;
-
-  // if (
-  //   selectedBannerQuestionId &&
-  //   (questionData?.type === QuestionType.SINGLE ||
-  //     questionData?.type === QuestionType.MULTI)
-  // ) {
-  // seriesData = bannerChartDataGen(
-  //   chartOptions.series,
-
-  // );
-  //}
-
-  // else {
-  //   if (questionData?.type === QuestionType.SINGLE) {
-  // seriesData = singleChartDataGen(series);
-  //console.log(seriesData);
-  //   } else if (questionData?.type === QuestionType.MULTI) {
-  //     seriesData = multiChartDataGen(chartOptions.series, chartLabelType);
-  //   } else if (questionData?.type === QuestionType.GRID) {
-  //     seriesData = gridChartTableGen(chartOptions.series, chartLabelType);
-  //   } else if (questionData?.type === QuestionType.RANK) {
-  //     seriesData = rankChartDataGen(chartOptions.series, chartLabelType);
-  //   } else if (questionData?.type === QuestionType.NUMBER) {
-  //     seriesData = numberChartDataGen(chartOptions.series, chartLabelType);
-  //   } else if (questionData?.type === QuestionType.GRID_MULTI) {
-  //     seriesData = multiGridChartDataGen(chartOptions.series, chartLabelType);
-  //   }
-  // }
-
   seriesData = MultiQuesExportUtils(newSeriesData);
-  // if (
-  //   newSeriesData.selectedBannerQuestionId &&
-  //   (newSeriesData.questionData?.type === QuestionType.SINGLE ||
-  //     newSeriesData.questionData?.type === QuestionType.MULTI)
-  // ) {
-  //   seriesData = bannerChartDataGen(newSeriesData);
-  // } else {
-  //   seriesData = singleChartDataGen(newSeriesData);
-  // }
-
-  //console.log('seriesData', seriesData);
 
   return seriesData;
 }
