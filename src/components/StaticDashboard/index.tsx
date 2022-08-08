b4617bd902b3652075a669be32bee89ac7855242
Sharawan Sharma <sharawan.sharma@e2eresearch.com>
2022-08-08 20:54:01 +0530

    Theme Update

--- src/components/StaticDashboard/index.tsx
+++ src/components/StaticDashboard/index.tsx
@@ -1,6 +1,6 @@
 import Objectives from '../Objectives';
 import Methodology from '../Methodology';
-// import backgroundWithMethodology from "../../assets/images/backgroun_+_methodiology.png"
+import DoshboardChart from "../../assets/svg/popup_icon.svg"
 
 import {
   CarouselProvider,
@@ -12,45 +12,42 @@ import {
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import BackgroundMethodology from '../BackgroundMethodology';
 import BusinessObjective from '../BusinessObjective';
+import { Grid } from '@material-ui/core';
 
 export interface StaticDashboardProps {
   onActionClick: () => void;
 }
 const StaticDashboard: React.FC<StaticDashboardProps> = (props) => {
   return (
-    <div>
-      {/* <img className="wid100" src={backgroundWithMethodology} /> */}
-      {/* <BackgroundMethodology /> */}
-      <Objectives />
+    
+    <div className="info-area">
+      <h2 className="info-area__heading">NielsenIQ BASES</h2>
+      <h3 className="info-area__subhead">Attribution Quad Maps</h3>
+      <span>Interpretation on headline results provided for detailed report</span>
+      <p>What are the key strengths and weaknesses of the product which matter to consumers?  Where are the opportunities for substantial performance improvement?</p>
+     
+      <Grid container>
+        <Grid item md={6} sm={6}>
+          <h3 className="info-area__subhead">Basic Benefits</h3>
+          <ul>
+            <li>I like that is low in fat</li>
+            <li>Fits well with the brand</li>
+            <li>60%+ C-level respondents​</li>
+            <li>Is suitable for the whole family</li>
+          </ul>
+          <h3 className="info-area__subhead">Key Strengths</h3>
+          <ul>
+            <li>Is made by a brand i trust</li>
+            <li>Is deal as a snack</li>
+            <li>i like that it contains corn</li>
+            <li>Is a light snack</li>
+          </ul>
+        </Grid>
+        <Grid item md={6} sm={6} className="info-area__imgwider">
+          <img src={DoshboardChart} />
+        </Grid>
+      </Grid>
     </div>
-    // <CarouselProvider
-    //   naturalSlideWidth={100}
-    //   naturalSlideHeight={125}
-    //   totalSlides={1}
-    //   isPlaying={true}
-    //   playDirection={"forward"}
-    //   interval={5000}
-    // >
-
-    //   <Slider>
-    //      <Slide className="slide" index={1}>
-    //      <img src={backgroundWithMethodology} />
-    //     {/* <Image hasMasterSpinner src={backgroundWithMethodology} /> */}
-    //     </Slide>
-    //     {/* <Slide className="slide" index={0}>
-    //       <Objectives />
-    //     </Slide> */}
-    //     {/* <Slide className="slide" index={1}>
-    //       <Methodology />
-    //     </Slide> */}
-    //   </Slider>
-    //   {/* <div className="dot-group"> */}
-
-    //   {/* <Dot slide={0} />
-    //   <Dot slide={1} /> */}
-    //   {/* </div> */}
-
-    // </CarouselProvider>
   );
 };
 
